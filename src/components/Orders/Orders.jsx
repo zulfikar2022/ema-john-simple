f1e18732b6396331ca53ccd0364c86bc4b021610
zulfikar2022 <sayedzulfikar2019@gmail.com>
2023-04-10 13:07:45 +0600

    I don't know what's new

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,52 +1,47 @@
-import React, { useState } from 'react';
-import './Orders.css';
-import Cart from '../Cart/Cart';
-import { useLoaderData, useSearchParams } from 'react-router-dom';
-import ReviewItem from '../ReviewItem/ReviewItem';
-import { deleteShoppingCart, removeFromDb } from '../../utilities/fakedb';
-import { Link } from 'react-router-dom';
-
+import React, { useState } from "react";
+import "./Orders.css";
+import Cart from "../Cart/Cart";
+import { useLoaderData, useSearchParams } from "react-router-dom";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import { deleteShoppingCart, removeFromDb } from "../../utilities/fakedb";
+import { Link } from "react-router-dom";
 
 const Orders = () => {
-    const savedCart = useLoaderData();
-    const [cart,setCart] = useState(savedCart);
+  const savedCart = useLoaderData();
+  const [cart, setCart] = useState(savedCart);
 
-    const handleRemoveFromCart = (id) => {
-        console.log(id);
-        const remaining = cart.filter(c => c.id!== id);
-        removeFromDb(id);
-        setCart(remaining);
-    }
-    const handleClearCart = () => {
-        setCart([]);
-        deleteShoppingCart();
-    }
-  
-    return (
-        <div className='shop-container'>
+  const handleRemoveFromCart = (id) => {
+    console.log(id);
+    const remaining = cart.filter((c) => c.id !== id);
+    removeFromDb(id);
+    setCart(remaining);
+  };
+  const handleClearCart = () => {
+    setCart([]);
+    deleteShoppingCart();
+  };
 
-            <div className='review-container'>
-                {
-                    cart.map(product => <ReviewItem 
-                        key={product.id}
-                        product={product} 
-                        handleRemoveFromCart={handleRemoveFromCart}
-                        ></ReviewItem>)
-                }
-            </div>
+  return (
+    <div className="shop-container">
+      <div className="review-container">
+        {cart.map((product) => (
+          <ReviewItem
+            key={product.id}
+            product={product}
+            handleRemoveFromCart={handleRemoveFromCart}
+          ></ReviewItem>
+        ))}
+      </div>
 
-            <div className='cart-container'>
-                <Cart
-                 cart = {cart}
-                 handleClearCart = {handleClearCart}
-                 >
-                    <Link to={'/checkout'}>
-                        <button className='proceed-checkout'>Proceed Checkout</button>
-                    </Link>
-                 </Cart>
-            </div>
-        </div>
-    );
+      <div className="cart-container">
+        <Cart cart={cart} handleClearCart={handleClearCart}>
+          <Link to={"/checkout"}>
+            <button className="proceed-checkout">Proceed Checkout</button>
+          </Link>
+        </Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
